bc9b256fbc0a18e4b9d1d44db27e4501130f156c
Seoyeon <eternalhymns@gmail.com>
2020-12-02 21:14:19 +0900

    structure of covid tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { MenuItem, FormControl, Select } from "material-ui/core";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/*BEM convention*/}
+      <h1>COVID-19 TRACKER</h1>
+      {/* FormControl, Select..etc... matarial ui offers!! */}
+      <FormControl className="app__dropdown">
+        {/* Select has a bunch of attributes */}
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option 2</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Yoooo</MenuItem>
+        </Select>
+      </FormControl>
+      {/*Header */}
+
+      {/*Title + Select input dropdown field */}
+
+      {/*InfoBox */}
+      {/*InfoBox */}
+      {/*InfoBox */}
+
+      {/*Table*/}
+      {/*Graph */}
+
+      {/*Map */}
     </div>
   );
 }
