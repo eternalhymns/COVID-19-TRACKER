cacce69b5a2305a8f821e51af0455a3e3e09b7a6
Seoyeon <eternalhymns@gmail.com>
2020-12-03 13:16:34 +0900

    Using state, completed dropdown menu( USA, UK,INDIA)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import "./App.css";
-import { MenuItem, FormControl, Select } from "material-ui/core";
+import React, { useState } from 'react';
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 function App() {
+  // STATE = How to write variable in REACT >>>>
+  const [countries, setCountries] = useState(['USA', 'UK', 'INDIA']) //the initial value is '[]'
   return (
     <div className="app">
-      {/*BEM convention*/}
-      <h1>COVID-19 TRACKER</h1>
-      {/* FormControl, Select..etc... matarial ui offers!! */}
-      <FormControl className="app__dropdown">
-        {/* Select has a bunch of attributes */}
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option 2</MenuItem>
-          <MenuItem value="worldwide">Option 3</MenuItem>
-          <MenuItem value="worldwide">Yoooo</MenuItem>
-        </Select>
-      </FormControl>
       {/*Header */}
+      <div className="app__header">
+        {/*BEM convention*/}
+        <h1>COVID-19 TRACKER</h1>
+        {/* FormControl, Select..etc... matarial ui offers!! */}
+        <FormControl className="app__dropdown">
+          {/* Select has a bunch of attributes */}
+          <Select variant="outlined" value="abc">
+            {/* Loop through all the countries and show a drop down list of the options */}
+            {
+              // every country return Item. in this case, return MenuItem
+              countries.map( country =>(
+              <MenuItem value={country}>{country}</MenuItem>
+              ))
+            }
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Option 3</MenuItem>
+            <MenuItem value="worldwide">Yoooo</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
 
       {/*Title + Select input dropdown field */}
 
